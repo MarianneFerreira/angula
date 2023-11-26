a0e768567e3843716c4e0b04d264fc92401007a4
Marianne de Andrade Ferreira <marianneaferreira@gmail.com>
2023-11-26 14:21:16 -0300

    Components

---
+++ angula/src/components/TituloDinamico.js
@@ -0,0 +1,28 @@
+class TituloDinamico extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+
+        //Base do component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = 'Olá'
+
+        // estilizar o coponent
+        const style = document.createElement("style");
+        style.textContent = `
+         h1{
+            color: red;
+         }
+        `
+
+        //enviar para o shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+
+    }
+}
+
+
+customElements.define("titulo-dinamico",TituloDinamico);
+
